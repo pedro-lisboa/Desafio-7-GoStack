932847d9efc5675c252e66d8b9fd2039fb151c8b
PEDRO LISBOA DE BARROS RODRIGUES TORRES <pegu_lisboa@hotmail.com>
2020-07-05 11:38:18 -0300

    desafio terminado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,16 +8,23 @@ import Logo from '../../assets/logo.svg';
 
 interface HeaderProps {
   size?: 'small' | 'large';
+  page?: 'dashboard' | 'import';
 }
 
-const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
+const Header: React.FC<HeaderProps> = ({
+  size = 'large',
+  page = 'dashboard',
+}: HeaderProps) => (
   <Container size={size}>
     <header>
       <img src={Logo} alt="GoFinances" />
       <nav>
-        {
-          // Todo
-        }
+        <Link to="/" className={page === 'dashboard' ? 'active' : ''}>
+          Listagem
+        </Link>
+        <Link to="/import" className={page === 'import' ? 'active' : ''}>
+          Importar
+        </Link>
       </nav>
     </header>
   </Container>
